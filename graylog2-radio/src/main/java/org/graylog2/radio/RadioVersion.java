8cacd2a5cc17652d94366baca7fb9861b4f08401
Kay Roepke <kroepke@googlemail.com>
2014-07-08 12:45:05 +0200

    use version.properties to build current version at runtime

--- graylog2-radio/src/main/java/org/graylog2/radio/RadioVersion.java
+++ graylog2-radio/src/main/java/org/graylog2/radio/RadioVersion.java
@@ -19,13 +19,17 @@
  */
 package org.graylog2.radio;
 
+import com.google.common.io.Resources;
 import org.graylog2.plugin.Version;
+
+import java.io.FileReader;
+import java.net.URL;
+import java.util.Properties;
 
 /**
  * @author Lennart Koopmann <lennart@torch.sh>
  */
 public class RadioVersion {
-
     public static final Version vDEV = new Version(0, 20, 0, "dev");
     public static final Version v0_20_0_PREVIEW_7 = new Version(0, 20, 0, "preview.7");
     public static final Version v0_20_0_RC_1 = new Version(0, 20, 0, "rc.1");
@@ -38,6 +42,25 @@ public class RadioVersion {
     public static final Version v0_20_2_RC_1 = new Version(0, 20, 2, "rc.1");
     public static final Version v0_20_2 = new Version(0, 20, 2);
 
-    public static final Version VERSION = v0_21_0_SNAPSHOT;
+    public static final Version VERSION;
     public static final String CODENAME = "Moose";
+
+    static {
+        Version tmpVersion;
+        try {
+            final URL resource = Resources.getResource("version.properties");
+            final FileReader versionProperties = new FileReader(resource.getFile());
+            final Properties version = new Properties();
+            version.load(versionProperties);
+            final int major = Integer.parseInt(version.getProperty("version.major", "0"));
+            final int minor = Integer.parseInt(version.getProperty("version.minor", "0"));
+            final int incremental = Integer.parseInt(version.getProperty("version.incremental", "0"));
+            final String qualifier = version.getProperty("version.qualifier", "unknown");
+            tmpVersion = new Version(major, minor, incremental, qualifier);
+        } catch (Exception e) {
+            tmpVersion = new Version(0, 0, 0, "unknown");
+            System.err.println("Unable to read version.properties file, this build has no version number.");
+        }
+        VERSION = tmpVersion;
+    }
 }
